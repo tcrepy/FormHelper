96ea360a99363c3cd0be4126cf4d7629f0d350a2
tcrepy <tanguycrepy@gmail.com>
2018-09-06 17:36:22 +0200

    FIX interface getTaille()

--- Fields/Textarea.php
+++ Fields/Textarea.php
@@ -4,6 +4,11 @@ namespace My_Helper\Form\Fields;
 
 class Textarea implements iField
 {
+    public function getTaille()
+    {
+        // TODO: Implement getTaille() method.
+    }
+
     public function getHtmlField()
     {
         // TODO: Implement getHtmlField() method.
