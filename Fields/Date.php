411fdd13c1e87dd2df96760bb92c1fb88abd1248
tcrepy <tanguycrepy@gmail.com>
2018-09-07 09:10:05 +0200

    FIX infinite loop in Date getValue()

--- Fields/Date.php
+++ Fields/Date.php
@@ -4,13 +4,13 @@ namespace My_Helper\Form\Fields;
 
 class Date extends Field
 {
-    public function __construct($name, $id, $class, $value, $placeholder, $taille = 3, $label)
+    public function __construct($name, $id, $class, $value, $taille = 3, $label)
     {
-        parent::__construct('date', $name, $id, $class, $value, $placeholder, $taille, $label);
+        parent::__construct('date', $name, $id, $class, $value, '', $taille, $label);
     }
 
     public function getValue()
     {
-        return strftime('%Y-%m-%d', strtotime(str_replace('/', '-', $this->getValue())));
+        return strftime('%Y-%m-%d', strtotime(str_replace('/', '-', $this->value)));
     }
 }
